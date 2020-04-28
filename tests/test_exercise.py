34b2a472f974ab16400a03f411185d10b9e607e7
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 11:14:15 +0100

    Updates exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,7 +1,33 @@
 import pytest
-from src.exercise import main
+import src.exercise
 
-def test_exercise(capsys):
-    main()
-    out, err = capsys.readouterr()
-    assert out == "\n", "Should read ''"
+def test_exercise():
+    input_values = ["3","6","8","2","45","45"]
+    output = []
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    assert [output[0],output[1],output[2]] == ["","","3 is smaller than 6."]
+    assert [output[3],output[4],output[5]] == ["","","8 is greater than 2."]
+    assert [output[6],output[7],output[8]] == ["","","45 is equal to 45."]
